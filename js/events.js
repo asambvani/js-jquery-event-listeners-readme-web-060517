b4b4fc47f51c854cc2fb6f893896e7d80013ad60
asambvani <alexsambvani@gmail.com>
2017-07-20 14:44:01 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -2,6 +2,36 @@
 
 $(document).ready(function(){
 
-// call functions here
+  getIt();
+  pressIt();
+  frameIt();
+  submitIt();
 
 });
+
+function getIt() {
+  $('p').on('click',()=>{
+    alert('Hey!')
+  })
+}
+
+function pressIt(){
+  $('input#typing').on('keydown',function(key){
+    if(key.which == 71){
+      alert('G was pressed!')
+    }
+  })
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+  });
+}
+
+function submitIt(){
+  $('form').on('submit',()=>{
+    console.log('Your form is going to be submitted now!')
+    alert('Your form is going to be submitted now.')
+  })
+}
